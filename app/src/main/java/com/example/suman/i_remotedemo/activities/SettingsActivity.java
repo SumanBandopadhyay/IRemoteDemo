2584b9416b4049fc44297ddd3c1fafc8b713ad2c
Suman Bandopadhyay <sumanbando006@gmai.com>
2018-04-08 00:00:39 +0530

    Design

--- app/src/main/java/com/example/suman/i_remotedemo/activities/SettingsActivity.java
+++ app/src/main/java/com/example/suman/i_remotedemo/activities/SettingsActivity.java
@@ -88,6 +88,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     };
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * Helper method to determine if the device has an extra-large screen. For
      * example, 10" tablets are extra-large.
@@ -133,6 +143,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        //actionBar.
     }
 
     /**
--- app/src/main/java/com/example/suman/i_remotedemo/fragments/WarehouseInventoryFragment.java
+++ app/src/main/java/com/example/suman/i_remotedemo/fragments/WarehouseInventoryFragment.java
@@ -2,29 +2,65 @@ package com.example.suman.i_remotedemo.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.suman.i_remotedemo.R;
+import com.example.suman.i_remotedemo.adapters.ItemAdapter;
+import com.example.suman.i_remotedemo.entity.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WarehouseInventoryFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private ItemAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private List<Item> items = new ArrayList<>();
 
     public WarehouseInventoryFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        populateData();
+    }
+
+    private void populateData() {
+        Item item = new Item();
+        item.setItemName("Item 1");
+        item.setItemCost(10);
+        items.add(item);
+
+        item = new Item();
+        item.setItemName("Item 2");
+        item.setItemCost(20);
+        items.add(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_warehouse_inventory, container, false);
+        View view = inflater.inflate(R.layout.fragment_warehouse_inventory, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.warehouse_recycler_view);
+        recyclerView.hasFixedSize();
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new ItemAdapter(items);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
 }
